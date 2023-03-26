3315145bc9721696d826565574d6cf8c8f4f35c8
Josue Ttito <josue@MacBook-Pro.local>
2023-03-25 21:52:12 -0500

    Initial Commit

---
+++ Autodoro/AutodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutodoroApp.swift
+//  Autodoro
+//
+//  Created by Josue Ttito on 25/03/23.
+//
+
+import SwiftUI
+
+@main
+struct AutodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
